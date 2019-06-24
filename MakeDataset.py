4eafdf79619a42576a04af6c4e273c1a226ddf06
Kai <38882183+ejlai@users.noreply.github.com>
2019-06-24 18:30:50 +0200

    Update MakeDataset.py

--- MakeDataset.py
+++ MakeDataset.py
@@ -18,17 +18,8 @@ fixed_size = tuple((500, 500))
 # path to training data
 train_path = "dataset/train"
 
-# no.of.trees for Random Forests
-num_trees = 100
-
 # bins for histogram
 bins = 8
-
-# train_test_split size
-test_size = 0.10
-
-# seed for reproducing same results
-seed = 9
 
 # feature-descriptor-1: Hu Moments
 def fd_hu_moments(image):
